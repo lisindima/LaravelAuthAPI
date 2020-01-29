b8ebe869a029eb61264922b32b80cd0ca4359b80
Lisin Dmitriy <lisindima1996@gmail.com>
2020-01-29 12:55:40 +0700

    Update pushnotification.php

--- config/pushnotification.php
+++ config/pushnotification.php
@@ -9,7 +9,7 @@ return [
   'fcm' => [
         'priority' => 'normal',
         'dry_run' => false,
-        'apiKey' => 'My_ApiKey',
+        'apiKey' => 'AAAAO1huez8:APA91bGFXOIfgQuHBBwku8HTy5q7-pHaCWVOLcDzsdsLZ7y16OO99tdgaaietxJ3zlmP5L8XYR5U9C_QfdQpsS7hxHpveYzBCtpbl86M9sCxHOKuF1BPVO595FISjoyGiwQ0dsjF6Ivp',
   ],
   'apn' => [
       'certificate' => __DIR__ . '/iosCertificates/apns-dev-cert.pem',
